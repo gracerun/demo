6ea606e6ed5c66866c36c584b7a1dae0c495f1fa
yaodan <yaodan_sn@sina.com>
2023-09-06 14:28:05 +0800

    java版本还原

--- producer-test/src/main/java/com/gracerun/message/service/OneServiceImpl.java
+++ producer-test/src/main/java/com/gracerun/message/service/OneServiceImpl.java
@@ -4,7 +4,7 @@ import com.gracerun.log.annotation.Logging;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-import jakarta.annotation.Resource;
+import javax.annotation.Resource;
 
 @Component
 @Slf4j
--- producer-test/src/main/java/com/gracerun/message/service/TwoServiceImpl.java
+++ producer-test/src/main/java/com/gracerun/message/service/TwoServiceImpl.java
@@ -5,7 +5,7 @@ import com.gracerun.log.annotation.ThrowableLog;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-import jakarta.annotation.Resource;
+import javax.annotation.Resource;
 
 @Component
 @Slf4j
